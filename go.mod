f12451c0f27d4b3465c0729324103bc95fa93f0c
Stanislav Ustiuzhanin <su@mobalyticshq.com>
2026-04-11 20:26:28 +0700

    fix: wip

--- go.mod
+++ go.mod
@@ -2,11 +2,19 @@ module github.com/ustasjs/gopher-markt
 
 go 1.25.7
 
-require go.uber.org/zap v1.27.1
+require (
+	github.com/go-chi/chi/v5 v5.2.5
+	github.com/jackc/pgx/v5 v5.9.1
+	go.uber.org/zap v1.27.1
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
-	github.com/stretchr/testify v1.11.1 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/sync v0.17.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
 )
---
+++ internal/router/router.go
@@ -0,0 +1,76 @@
+package router
+
+import (
+	"compress/gzip"
+	"database/sql"
+	"github.com/ustasjs/gopher-markt/internal/config/settings"
+	"github.com/ustasjs/gopher-markt/internal/logger"
+	customMiddleware "github.com/ustasjs/gopher-markt/internal/middleware"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"go.uber.org/zap"
+)
+
+func StartServer() {
+	settingsMap := settings.InitSettings()
+	loggerErr := logger.Initialize(settingsMap.LogLevel)
+	if loggerErr != nil {
+		panic(loggerErr)
+	}
+
+	var db *sql.DB
+	if settingsMap.DatabaseURI != "" {
+		var dbErr error
+		db, dbErr = sql.Open("pgx", string(settingsMap.DatabaseURI))
+
+		logger.Log.Info("Connect to database")
+
+		if dbErr != nil {
+			panic(dbErr)
+		}
+		defer db.Close()
+
+		// TODO add migrations
+		//migrationsErr := migrations.RunMigrations(db)
+		//if migrationsErr != nil {
+		//	panic(migrationsErr)
+		//}
+	}
+
+	logger.Log.Info("Starting server on:", zap.String("address", string(settingsMap.ServerAddress)))
+
+	// TODO add storage
+
+	r := chi.NewRouter()
+	initMiddleware(r, store)
+	initRoutes(r, settingsMap, db)
+
+	srv := &http.Server{
+		Addr:              string(settingsMap.ServerAddress),
+		Handler:           r,
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       120 * time.Second,
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func initRoutes(r *chi.Mux, s *settings.Settings, db *sql.DB) {
+	// TODO add handlers
+}
+
+func initMiddleware(r *chi.Mux, store customMiddleware.UserRepository) {
+	r.Use(logger.LoggerMiddleware)
+	r.Use(customMiddleware.GzipDecompress)
+	r.Use(middleware.Compress(gzip.DefaultCompression))
+	r.Use(customMiddleware.Auth(store))
+}
