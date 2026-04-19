a648041298c2cd818fc3ebbb737db6b5bab1144c
Stanislav Ustiuzhanin <su@mobalyticshq.com>
2026-04-19 17:21:14 +0700

    fix: add extra logs

--- internal/config/settings/utils.go
+++ internal/config/settings/utils.go
@@ -11,13 +11,13 @@ import (
 func validateBaseServerAddress(serverAddress string, errorMessage string) error {
 	value := strings.Split(serverAddress, ":")
 	if len(value) != 2 {
-		logger.Log.Warn("serverAddress format is wrong", zap.String("serverAddress", serverAddress))
+		logger.Log.Info("serverAddress format is wrong", zap.String("serverAddress", serverAddress))
 		return errors.New(errorMessage)
 	}
 	host := value[0]
 	port := value[1]
 	if host == "" || port == "" {
-		logger.Log.Warn("host aor port are empty", zap.String("serverAddress", serverAddress), zap.String("host", host), zap.String("port", port))
+		logger.Log.Info("host or port are empty", zap.String("serverAddress", serverAddress), zap.String("host", host), zap.String("port", port))
 		return errors.New(errorMessage)
 	}
 	return nil
