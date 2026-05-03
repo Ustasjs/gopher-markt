c17c277f2a3612daccde29b3bbd4df0bf1595ac6
Stanislav Ustiuzhanin <su@mobalyticshq.com>
2026-05-03 19:46:39 +0700

    fix: text to varchar

--- migrations/000003_create_orders_table.up.sql
+++ migrations/000003_create_orders_table.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS orders (
     id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    number      TEXT NOT NULL UNIQUE,
+    number      VARCHAR(50) NOT NULL UNIQUE,
     user_id     UUID NOT NULL REFERENCES users(id),
-    status      TEXT NOT NULL DEFAULT 'NEW',
+    status      VARCHAR(20) NOT NULL DEFAULT 'NEW',
     accrual     BIGINT NULL,
     uploaded_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
