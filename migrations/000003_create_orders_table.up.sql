e9ac6fdfbb735cfe5c3aa2df52c96b4f4de0b443
Stanislav Ustiuzhanin <su@mobalyticshq.com>
2026-05-03 19:49:35 +0700

    fix: enum for status

--- migrations/000003_create_orders_table.up.sql
+++ migrations/000003_create_orders_table.up.sql
@@ -1,8 +1,10 @@
+CREATE TYPE order_status AS ENUM ('NEW', 'PROCESSING', 'PROCESSED', 'INVALID');
+
 CREATE TABLE IF NOT EXISTS orders (
     id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     number      VARCHAR(50) NOT NULL UNIQUE,
     user_id     UUID NOT NULL REFERENCES users(id),
-    status      VARCHAR(20) NOT NULL DEFAULT 'NEW',
+    status      order_status NOT NULL DEFAULT 'NEW',
     accrual     BIGINT NULL,
     uploaded_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
